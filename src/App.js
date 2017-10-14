8fe7b7b8227e04adc28f40e97456a6c745d715bd
Alisson Suassuna <alissonsuassuna@gmail.com>
2017-10-14 13:08:36 -0300

    criando um componente reutilizavel

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 import './css/pure-min.css';
 import './css/side-menu.css';
 
+import ImputCustomizado from './componentes/InputCustomizado';
+
 class App extends Component {
 
   constructor(){
@@ -35,8 +37,10 @@ class App extends Component {
        type:'post',
        data: JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
        success: function(resposta){
+         console.log(resposta);
+         this.setState({lista:resposta});
          console.log("enviado com sucesso");
-      },
+      }.bind(this),
        error: function(resposta){
          console.log("erro");
        }      
@@ -57,7 +61,7 @@ class App extends Component {
 
   render() {
     return (
-      <div id="layout">
+      <main id="layout">
         <a href="#menu" id="menuLink" className="menu-link">
             <span></span>
         </a>
@@ -79,18 +83,11 @@ class App extends Component {
             <div className="content" id="content">
               <div className="pure-form pure-form-aligned">
                 <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
-                  <div className="pure-control-group">
-                    <label htmlFor="nome">Nome</label> 
-                    <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome}/>                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="email">Email</label> 
-                    <input id="email" type="email" name="email" value={this.state.email}  onChange={this.setEmail}/>                  
-                  </div>
-                  <div className="pure-control-group">
-                    <label htmlFor="senha">Senha</label> 
-                    <input id="senha" type="password" name="senha" value={this.state.senha}  onChange={this.setSenha}/>                                      
-                  </div>
+
+                <ImputCustomizado id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} label="Nome"/>                                              
+                <ImputCustomizado id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} label="Email"/>                                              
+                <ImputCustomizado id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} label="Senha"/>
+          
                   <div className="pure-control-group">                                  
                     <label></label> 
                     <button type="submit" className="pure-button pure-button-primary">Gravar</button>                                    
@@ -122,7 +119,7 @@ class App extends Component {
               </div>             
             </div>
           </div>  
-      </div>
+      </main>
     );
   }
 }
